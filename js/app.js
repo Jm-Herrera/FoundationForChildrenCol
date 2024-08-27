86d11ad81ed8566ece624b22df24ff4b9cfcc0fa
JuanchoRamon <jlugogalan1@ncstudents.niagaracollege.ca>
2024-08-26 23:31:38 -0400

    Primer Push

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).foundation()
+
+var $navList = $('.menu-fancy');
+
+$navList.on('click', 'li:not(.is-selected)', function(e){
+  $navList.find(".is-selected").removeClass("is-selected");
+  $(e.currentTarget).addClass("is-selected");
+});
+
