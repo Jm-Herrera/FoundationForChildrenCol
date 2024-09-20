11baa672d1d62875a3a14aaaacb6f86500c554fb
JuanchoRamon <jlugogalan1@ncstudents.niagaracollege.ca>
2024-09-20 02:03:26 -0400

    update Programs,home,About us

--- js/app.js
+++ js/app.js
@@ -1,9 +1,1 @@
 $(document).foundation()
-
-var $navList = $('.menu-fancy');
-
-$navList.on('click', 'li:not(.is-selected)', function(e){
-  $navList.find(".is-selected").removeClass("is-selected");
-  $(e.currentTarget).addClass("is-selected");
-});
-
